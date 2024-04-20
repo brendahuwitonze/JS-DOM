7bc9b0917b786b494517bfde004a8d2f3fec02e7
brendahuwitonze <uwitonzebrendah@gmail.com>
2024-04-20 15:04:04 +0200

    added anothe row to the card and added some function to the card

--- script.js
+++ script.js
@@ -1,17 +1,48 @@
 
-
 function attachBuyEvents() {
   const btns = document.querySelectorAll('#products .buy')
   for (const btn of btns) {
     btn.addEventListener('click',function(e){
      const parentName = btn.parentElement;
-    const input = parentName.querySelector('input')
-    const inputValue = input.value
-console.log(parentName.textContent + inputValue)
-    //  const specifiedAttribute=  parentName.getAttribute('data-id')
      parentName.classList.toggle('sale')
+    //get the id
+    const specifiedAttribute=  parentName.getAttribute('data-id')
+    //get the product name
+    const h2 = e.target.parentElement.querySelector('h2');
+    const text = h2.textContent
+    // get quantity
+    const input = parentName.querySelector('input')
+    const inputValue = parseInt(input.value)
+    //get price
+    const price = e.target.parentElement.querySelector('p')
+    const textprice = parseInt(price.textContent)
+    //appending the row to the table  and adding the functionality 
+    const table = document.querySelector(' #cart table')
+    const tbody = document.createElement('tbody')
+    table.appendChild(tbody)
+    //appending the columns to the row and adding some text into the columns coresspondingly
+const newRow = document.createElement('tr')
+tbody.appendChild(newRow)
+const th1 = document.createElement('th')
+newRow.append(th1)
+th1.textContent = specifiedAttribute
+const th2 = document.createElement('th')
+newRow.append(th2)
+th2.textContent =text;
+const th3 = document.createElement('th')
+newRow.append(th3)
+th3.textContent = inputValue
+const th4 = document.createElement('th')
+newRow.append(th4)
+th4.textContent =  textprice
+const th5 = document.createElement('th')
+newRow.append(th5)
+//Multipying quantity with price to find total 
+th5.textContent = th4.textContent * th3.textContent
   })
 }
+// console.log(parentName.textContent + inputValue)
+
 }
 attachBuyEvents()
 
